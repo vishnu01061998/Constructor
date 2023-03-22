b72d2433d46726c448e455bf52c64c0966928449
HP <HP@DESKTOP-4Q8V1JF>
2023-03-23 02:09:27 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Employee
+    {
+
+        public  int Id { get; set; }   
+        public string Name { get; set; }
+        public int Salary { get; set; }
+
+        public static int IncomeTax { get; set; }
+
+        public Employee() // parameterless or default constructor
+        {
+           this .Id = 10;
+           this. Name = "Vaish";
+           this. Salary = 10000;
+        }
+
+
+        public Employee( int id, string name,int salary)//parameterized constructor
+        {
+            this .Id = id;
+            this. Name = name;
+            this.Salary = salary;
+        }
+
+        public Employee(Employee emp ) // copy construstor 
+        {
+            this.Id= emp.Id;
+            this. Name = emp.Name;
+            this.Salary = emp.Salary;
+        }
+
+
+        public void Showdata()
+        {
+            Console.WriteLine("Id=" + this.Id);
+            Console.WriteLine("Name=" + this.Name);
+            Console.WriteLine("salary=" + this.Salary);
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("Id=" + this.Id);
+            Console.WriteLine("Name=" + this.Name);
+            Console.WriteLine("Salary=" + this.Salary);
+        }
+      
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Constructor
+{
+    using Constructor;
+    public class Program
+    {
+         public static void Main()
+         {
+            Employee emp1 = new Employee();
+            
+            Employee emp2 = new Employee(11,"Vijay",20000); // call to parameterless consturctor
+
+            Employee emp3 = new Employee(12, "Vishnu", 30000);// call to parameterized constructor 
+
+            Employee emp2Copy = new Employee(emp2);
+
+           
+
+            emp1.ShowData();
+            emp2.ShowData();
+            emp3.ShowData();
+            emp2Copy.ShowData();
+
+
+            
+
+         } 
+    }
+}
